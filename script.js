742506d3f2656b5042b9f52527c77c8b32cab1f9
juanrangel2 <143430673+juanrangel2@users.noreply.github.com>
2024-04-25 16:13:23 +0000

    first

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 // Declare your arrays here
 
+let pic = document.querySelector('#pic');
+let sweets = ["cin2.webp", "pan3.jpg"];
 // Make sure to declare your HTML elements as variables! 
 
 // Submit Button 
 let submitButton = document.querySelector("button");
-submitButton.addEventListener("click", function() {});
+submitButton.addEventListener("click", function() {
+
+let user = document.querySelector("input").value;
+if (user === "Sweet"){
+    sweets.forEach(function(sweets){
+    let image = document.createElement("img");
+    image.src = sweets;
+pic.appendChild(image);
+    });
+}
+});
+
